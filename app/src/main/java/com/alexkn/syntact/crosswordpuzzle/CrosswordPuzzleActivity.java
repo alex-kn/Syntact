7f3c6e67b501fc40859fc5c7454c14b8e325ddc3
Unknown <alx.kntl@gmail.com>
2018-08-29 21:38:30 +0200

    Initial commit

---
+++ app/src/main/java/com/alexkn/syntact/crosswordpuzzle/CrosswordPuzzleActivity.java
@@ -0,0 +1,15 @@
+package com.alexkn.syntact.crosswordpuzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alexkn.syntact.R;
+
+public class CrosswordPuzzleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crossword_puzzle_activity);
+    }
+}
