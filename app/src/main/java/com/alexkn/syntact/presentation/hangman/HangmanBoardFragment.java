cb27c54118e91037d7619c28bb97406dc1396801
Alex <alx.kntl@gmail.com>
2019-04-14 12:09:18 +0200

    dependencies

--- app/src/main/java/com/alexkn/syntact/presentation/hangman/HangmanBoardFragment.java
+++ app/src/main/java/com/alexkn/syntact/presentation/hangman/HangmanBoardFragment.java
@@ -74,9 +74,9 @@ public class HangmanBoardFragment extends Fragment {
         letterListAdapter2 = new LetterListAdapter();
         letterViewRight.setAdapter(letterListAdapter2);
 
-
         new Handler().postDelayed(() -> {
-            viewModel.getLettersLeft().observe(getViewLifecycleOwner(), letterListAdapter1::submitList);
+            viewModel.getLettersLeft()
+                    .observe(getViewLifecycleOwner(), letterListAdapter1::submitList);
             viewModel.getLettersRight()
                     .observe(getViewLifecycleOwner(), letterListAdapter2::submitList);
             viewModel.getSolvablePhrases(languagePairId)
