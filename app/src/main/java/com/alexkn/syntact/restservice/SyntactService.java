a60dc48d5c29890478e887476491c46d5bdc9ff8
Alex <alx.kntl@gmail.com>
2019-06-11 21:44:49 +0200

    Add delete templates to service

--- app/src/main/java/com/alexkn/syntact/restservice/SyntactService.java
+++ app/src/main/java/com/alexkn/syntact/restservice/SyntactService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
@@ -38,4 +39,7 @@ public interface SyntactService {
 
     @POST("templates/")
     Call<Template> postTemplate(@Header("Authorization") String token, @Body TemplateRequest templateRequest);
+
+    @DELETE("templates/{id}")
+    Call<ResponseBody> deleteTemplate(@Header("Authorization") String token, @Path("id") Long templateId);
 }
